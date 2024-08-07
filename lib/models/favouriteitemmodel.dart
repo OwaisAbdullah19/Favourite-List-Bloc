9bb3c0127dd1d9a43fae24ea16794e3bb3427783
Owais Abdullah <owaisabdullah19@gmail.com>
2024-08-08 00:21:59 +0500

    Model

---
+++ lib/models/favouriteitemmodel.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+class FavouriteItem extends Equatable{
+ final String id;
+ final  String value;
+final bool isdeleting;
+ final bool isfavourite;
+FavouriteItem({
+  required this.id,
+  required this.value,
+  this.isfavourite =true,
+  this.isdeleting =false,
+  });
+  FavouriteItem copyWith({String?id,String?value,bool?isdeleting,bool?isfavourite}){
+
+      return FavouriteItem(
+        id: id??this.id, 
+        value: value??this.value,
+        isfavourite: isfavourite??this.isfavourite,
+        isdeleting: isdeleting??this.isdeleting);
+    }
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id,value,isdeleting,isfavourite];
+
+
+
+}
