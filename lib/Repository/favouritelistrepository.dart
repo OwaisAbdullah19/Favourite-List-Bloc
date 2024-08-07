7a57da1d33a38d5098b12aaaad10fc3f98822733
Owais Abdullah <owaisabdullah19@gmail.com>
2024-08-08 00:28:13 +0500

    Repository

---
+++ lib/Repository/favouritelistrepository.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:favouritelistbloc/models/favouriteitemmodel.dart';
+
+class Favouritelistrepository {
+ 
+ Future<List<FavouriteItem>> fetchlist()async{
+    await Future.delayed(Duration(seconds: 3));
+    return List.generate(3, (generator) =>FavouriteItem(id: generator.toString(), value: ('Item $generator')));
+
+ }
+}
+ 
+ 
+ 
+
+
+
+
